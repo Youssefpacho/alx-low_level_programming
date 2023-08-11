5a797aed2b5c7510d29cc8421a9e653c49f66dcf
Youssefpacho <benbrahim.youssef97@gmail.com>
2023-08-10 17:01:28 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Description: Uses the puts function to print a message
+ *
+ * Return: Always 0 (Success)
+ */
 #include<stdio.h>
-int main(){
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
