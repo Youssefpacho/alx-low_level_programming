c9561fcc707636bd74e4809f6deda54f25dc73de
Youssefpacho <benbrahim.youssef97@gmail.com>
2023-08-15 13:48:43 -0700

    README.md

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints _putchar followed by a new line
+ * Return: 0 (success)
+ **/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the file containing the tasks 0x02 of ALX Course
