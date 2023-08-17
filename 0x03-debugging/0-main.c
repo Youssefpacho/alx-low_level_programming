e23748c128931bc35ebd1a57fc54bde88f2a598d
Youssefpacho <benbrahim.youssef97@gmail.com>
2023-08-17 15:13:58 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,3 @@
-#include<stdio.h>
-#include<time.h>
-#include<stdlib>
 #include "main.h"
 
 /**
@@ -10,10 +7,10 @@
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
+        i = 98;
+        positive_or_negative(i);
 
-	positive_or_negative(i);
-	return (0);
+        return (0);
 }
