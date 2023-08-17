5fbd530bfc40182a3fa6b8b90313840cec3b67e8
Youssefpacho <benbrahim.youssef97@gmail.com>
2023-08-17 15:12:29 -0700

    0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * positive_negative - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+
+void positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+}
